6a1bec814c0d5926e10e253451d09f1c48bde885
BilalsGituation <billyasser@hotmail.co.uk>
2022-06-12 18:23:40 +0200

    output hopefully more readable

--- Simulations/BasicCallCentre_sim.py
+++ Simulations/BasicCallCentre_sim.py
@@ -14,8 +14,7 @@ NUM_EMPLOYEES=5
 #Simpy clock runs in minutes
 MEAN_SUPPORT=5
 BETWEEN_CUSTOMERS=2 # time between customers
-END_T = 435 # 120 in the tutorial but 2-hour shifts unrealistic, no?
-# So we have an 8h day with 45m employee break seamlessly included
+END_T = 240 
 
 # counter placeholders
 n_supported = 0
@@ -34,7 +33,7 @@ class CallCentre:
     def support(self, customer):
         random_time = max(1, np.random.normal(self.duration, 4))
         yield self.env.timeout(random_time) # generator
-        print(f"Customer support concluded for customer {customer} at {self.env.now:.2f} minutes into shift.")
+        print(f"Customer support concluded for customer {customer} at {self.env.now:.2f} minutes into shift.\n")
 
 
 # The function that happens (while I would love to leave it here...)
@@ -43,12 +42,12 @@ class CallCentre:
 def customer(env, name, call_centre):
     global n_supported, n_waiting
     n_waiting+=1
-    print(f"Customer {name} enters waiting queue at {env.now:.2f} minutes into shift.")
+    print(f"Customer {name} enters waiting queue at {env.now:.2f} minutes into shift.\n")
     with call_centre.staff.request() as request:
         yield request
-        print(f"Customer {name} enters call at {env.now:.2f} minutes into shift.")
+        print(f"Customer {name} enters call at {env.now:.2f} minutes into shift.\n")
         yield env.process(call_centre.support(name))
-        print(f"Customer {name} left call at {env.now:.2f} minutes into shift.")
+        print(f"Customer {name} left call at {env.now:.2f} minutes into shift.\n")
         n_supported+=1
         n_waiting-=1
 
@@ -70,26 +69,26 @@ sample_supported = []
 sample_unsupported = []
 
 #Run the shift several times
-for i in range(100):
+for i in range(50):
     # counters for customer state at end of day
     n_supported=0
     n_waiting=0
-    print(f"Starting sim {i+1} of shift at call centre")
+    print(f"Starting sim {i+1} of shift at call centre\n")
     #run the simulation once
     env = simpy.Environment()
     env.process(setup(env, NUM_EMPLOYEES, MEAN_SUPPORT, BETWEEN_CUSTOMERS))
     env.run(until=END_T)
     #description at end of shift
-    print(f"Number of customers supported in shift {i + 1}: {n_supported}.")
-    print(f"Number of customers unsupported in shift {i + 1}: {n_waiting}.")
+    print(f"Number of customers supported in shift {i + 1}: {n_supported}.\n")
+    print(f"Number of customers unsupported in shift {i + 1}: {n_waiting}.\n")
     #feed generated data into variables and report/do mild stats at end of day
     total_supported = total_supported + n_supported
     sample_supported.append(n_supported)
-    mean_sd_supported = f"Mean daily total customers supported ({i + 1} days) = {total_supported/(i+1)}, S.D. = {np.std(sample_supported)}"
-    print(f"Cumulative total customers supported (Day {i + 1}): {total_supported}.")
-    print(f"{mean_sd_supported}")
+    mean_sd_supported = f"Mean daily total customers supported ({i + 1} days) = {total_supported/(i+1)}, S.D. = {np.std(sample_supported)}.\n"
+    print(f"Cumulative total customers supported (Day {i + 1}): {total_supported}.\n")
+    print(f"{mean_sd_supported}\n")
     left_waiting = left_waiting + n_waiting
     sample_unsupported.append(n_waiting)
-    mean_sd_unsupported = f"Mean daily total customers left unsupported ({i + 1} days) = {left_waiting/(i+1)}, S.D. = {np.std(sample_unsupported)}"
-    print(f"Cumulative total customers whose issues were not resolved on day of call (Day {i+1}): {left_waiting}.")
-    print(f"{mean_sd_unsupported}")
+    mean_sd_unsupported = f"Mean daily total customers left unsupported ({i + 1} days) = {left_waiting/(i+1)}, S.D. = {np.std(sample_unsupported)}.\n"
+    print(f"Cumulative total customers whose issues were not resolved on day of call (Day {i+1}): {left_waiting}.\n")
+    print(f"{mean_sd_unsupported}\n")
