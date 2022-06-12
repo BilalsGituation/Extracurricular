cf4415e58e1a74b40a6f53b9e2f44229bca96620
BilalsGituation <billyasser@hotmail.co.uk>
2022-06-12 18:02:23 +0200

    hope more readable

--- Simulations/BasicCallCentre_sim.py
+++ Simulations/BasicCallCentre_sim.py
@@ -2,29 +2,34 @@
 
 # This will be based off anything I can find using the search term "simpy"
 # on YouTube, but uses @NeuralNine's 18-minute video found on the first page
+# as jumpoff
 
 import random
 import numpy as np
 import simpy
 import statistics
 
-NUM_EMPLOYEES=5 # changing some names to show I didn't paste this from somewhere
+#Resource count
+NUM_EMPLOYEES=5
+#Simpy clock runs in minutes
 MEAN_SUPPORT=5
-BETWEEN_CUSTOMERS=2
+BETWEEN_CUSTOMERS=2 # time between customers
 END_T = 435 # 120 in the tutorial but 2-hour shifts unrealistic, no?
 # So we have an 8h day with 45m employee break seamlessly included
 
-n_supported = 0 # i don't want to write my vars in caps, lesson learned
+# counter placeholders
+n_supported = 0
 n_waiting = 0
 total_supported = 0
 left_waiting = 0
 
+# class
 class CallCentre:
-
+# initial state
     def __init__(self, env, staff, duration):
-        self.env = env
-        self.staff = simpy.Resource(env,staff)
-        self.duration = duration
+        self.env = env # simulation needs an environment
+        self.staff = simpy.Resource(env,staff) # employee is a simulation resource
+        self.duration = duration # call duration is called in downstream function
 
     def support(self, customer):
         random_time = max(1, np.random.normal(self.duration, 4))
@@ -32,6 +37,7 @@ class CallCentre:
         print(f"Customer support concluded for customer {customer} at {self.env.now:.2f} minutes into shift.")
 
 
+# The function that happens
 def customer(env, name, call_centre):
     global n_supported, n_waiting
     n_waiting+=1
@@ -44,7 +50,8 @@ def customer(env, name, call_centre):
         n_supported+=1
         n_waiting-=1
 
-
+# the function that connects a customer to the employee and decides its duration
+# based on yields from customer(?)
 def setup(env, staff, duration, interval):
     call_centre = CallCentre(env, staff, duration)
 
@@ -56,24 +63,26 @@ def setup(env, staff, duration, interval):
             i+=1
             env.process(customer(env, i, call_centre))
 
+#placeholder for our sample
 sample_supported = []
 sample_unsupported = []
 
-#unsupported = n_waiting - n_supported
+#Run the shift several times
 for i in range(100):
+    # counters for customer state at end of day
     n_supported=0
     n_waiting=0
     print(f"Starting sim {i+1} of shift at call centre")
+    #run the simulation once
     env = simpy.Environment()
     env.process(setup(env, NUM_EMPLOYEES, MEAN_SUPPORT, BETWEEN_CUSTOMERS))
-#print(f"Number of unsupported customers in queue: {name - n_supported}.")
-#print(f"Number of customers mid-call: {name - n_supported}.")
     env.run(until=END_T)
+    #description at end of shift
     print(f"Number of customers supported in shift {i + 1}: {n_supported}.")
     print(f"Number of customers unsupported in shift {i + 1}: {n_waiting}.")
+    #feed generated data into variables and report/do mild stats at end of day
     total_supported = total_supported + n_supported
     sample_supported.append(n_supported)
-    #print(sample_supported)
     mean_sd_supported = f"Mean daily total customers supported ({i + 1} days) = {total_supported/(i+1)}, S.D. = {np.std(sample_supported)}"
     print(f"Cumulative total customers supported (Day {i + 1}): {total_supported}.")
     print(f"{mean_sd_supported}")
