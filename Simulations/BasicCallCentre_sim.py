9f048079a236d67c07bcb4b57357ceb9c1bc146e
BilalsGituation <billyasser@hotmail.co.uk>
2022-06-12 16:03:45 +0200

    sim CNDset1 run 3 times

--- Simulations/BasicCallCentre_sim.py
+++ Simulations/BasicCallCentre_sim.py
@@ -14,6 +14,7 @@ END_T = 435 # 120 in the tutorial but 2-hour shifts unrealistic, no?
 # So we have an 8h day with 45m employee break seamlessly included
 
 n_supported = 0 # i don't want to write my vars in caps, lesson learned
+n_waiting = 0
 
 class CallCentre:
 
@@ -25,30 +26,67 @@ class CallCentre:
     def support(self, customer):
         random_time = max(1, np.random.normal(self.duration, 4))
         yield self.env.timeout(random_time) # generator
-        print(f"Customer support concluded for customer {customer} at minute {self.env.now:.2f} of the shift.")
+        print(f"Customer support concluded for customer {customer} at {self.env.now:.2f} minutes into shift.")
+
 
 def customer(env, name, call_centre):
-    global n_supported
-    print(f"Customer {name} enters waiting queue at minute {env.now:.2f} of the shift.")
+    global n_supported, n_waiting
+    n_waiting+=1
+    print(f"Customer {name} enters waiting queue at {env.now:.2f} minutes into shift.")
     with call_centre.staff.request() as request:
         yield request
-        print(f"Customer {name} enters call centre at minute {env.now:.2f} of the shift.")
+        print(f"Customer {name} enters call at {env.now:.2f} minutes into shift.")
         yield env.process(call_centre.support(name))
-        print(f"Customer {name} left call at minute {env.now:.2f} of the shift.")
+        print(f"Customer {name} left call at {env.now:.2f} minutes into shift.")
         n_supported+=1
+
 
 def setup(env, staff, duration, interval):
     call_centre = CallCentre(env, staff, duration)
 
     for i in range(1,6):
-        env.process(customer(env, staff, call_centre))
+        env.process(customer(env, i, call_centre))
 
         while True:
             yield env.timeout(random.randint(interval -1, interval+1))
             i+=1
             env.process(customer(env, i, call_centre))
 
+#unsupported = n_waiting - n_supported
+
 print("Starting sim of shift at call centre")
 env = simpy.Environment()
 env.process(setup(env, NUM_EMPLOYEES, MEAN_SUPPORT, BETWEEN_CUSTOMERS))
+#print(f"Number of unsupported customers in queue: {name - n_supported}.")
+#print(f"Number of customers mid-call: {name - n_supported}.")
 env.run(until=END_T)
+print(f"Number of customers supported: {n_supported}.")
+print(f"Number of customers unsupported: {n_waiting}.")
+
+
+'''
+Terminal: python BasicCallCentre_sim.py > ./CallCentre_output.txt
+
+CONDS 1:
+NUM_EMPLOYEES=2 # changing some names to show I didn't paste this from somewhere
+MEAN_SUPPORT=5
+BETWEEN_CUSTOMERS=2
+END_T = 435
+
+CONDS 1 RUN 1:
+Number of customers supported: 159.
+Number of customers unsupported: 218.
+
+CONDS 1 RUN 2:
+Number of customers supported: 165.
+Number of customers unsupported: 217.
+
+
+CONDS 1 RUN 3:
+Number of customers supported: 163.
+Number of customers unsupported: 222.
+# '''
+# Looks like this call centre wouldn't be very highly regarded as is. Now that we
+# know some basics, let's just change some conditions to play around with this and
+# move on, as I would like an efficient way to run a sim many times and get summary
+# stats, rather than optimise this much more
