c863387ddb61ff84a23376339fd9a69018771c16
BilalsGituation <billyasser@hotmail.co.uk>
2022-06-14 07:51:27 +0200

    mostly cosmetic fixes

--- Simulations/MorningBirds_sim.py
+++ Simulations/MorningBirds_sim.py
@@ -9,6 +9,9 @@
 
 '''
 Credit to GitHub@fitosky1 for starting the line of joking that led to this script!
+
+please see history files in this repo "BasicCallCentre.py" and its output
+"CallCentre_output.txt"
 '''
 
 import random
@@ -17,7 +20,7 @@ import simpy
 import statistics
 
 #Resource count
-Songbird_capacity=5
+Songbird_capacity=2 # changed to 2 perches on tree
 #Simpy clock runs in minutes
 MEAN_SING=5
 BETWEEN_BIRDS=2 # time between customers
@@ -34,8 +37,8 @@ class TreeNearWindow:
 # initial state
     def __init__(self, env, perch, duration):
         self.env = env # simulation needs an environment
-        self.staff = simpy.Resource(env,perch) # employee is a simulation resource
-        self.duration = duration # call duration is called in downstream function
+        self.staff = simpy.Resource(env,perch) # space for bird to sing is a simulation resource
+        self.duration = duration # song duration is called in downstream function
 
     def song(self, bird):
         random_time = max(1, np.random.normal(self.duration, 4))
@@ -45,29 +48,27 @@ class TreeNearWindow:
         else:
             print(f"Bird {bird} finished singing at {self.env.now:.2f} minutes after 4:00AM, flew off somewhere.\n")
 
-# The function that happens (while I would love to leave it here...)
-# and causes generation of customer, to go through the phone interaction
-# in setup function
+# The function that
+#  causes generation of birds, to go through the singing queue in setup function
 def bird(env, name, nearby_tree):
     global n_finished_song, n_waiting
     n_waiting+=1
     if env.now > 10:
-        print(f"Bird {name} sits on tree perch at 4:{env.now:.2f}AM.\n")
+        print(f"Bird {name} starts hovering around tree perch at 4:{env.now:.2f}AM.\n")
     else:
-        print(f"Bird {name} sits on tree perch at {env.now:.2f} minutes after 4:00AM.\n")
+        print(f"Bird {name} starts hovering around tree perch at {env.now:.2f} minutes after 4:00AM.\n")
     with nearby_tree.staff.request() as request:
         yield request
         if env.now > 10:
-            print(f"Bird {name} starts singing at 4:{env.now:.2f}.\n")
+            print(f"Bird {name} is able to sit and starts singing at 4:{env.now:.2f}.\n")
         else:
-            print(f"Bird {name} starts singing at {env.now:.2f} minutes after 4:00AM.\n")
+            print(f"Bird {name} is able to sit and starts singing at {env.now:.2f} minutes after 4:00AM.\n")
         yield env.process(nearby_tree.song(name))
-        #print(f"Customer {name} left call at {env.now:.2f} minutes into shift.\n")
         n_finished_song+=1
         n_waiting-=1
 
-# the function that connects a customer to the employee and decides its duration
-# based on yields from customer(?)
+# the function that connects a bird to a perch and decides its song duration
+# after receiving yields from bird function(?)
 def setup(env, perch, duration, interval):
     nearby_tree = TreeNearWindow(env, perch, duration)
 
@@ -85,15 +86,15 @@ sample_LeftWaiting = []
 
 #Run the shift several times
 for i in range(50):
-    # counters for customer state at end of day
+    # counters for bird state at end of day
     n_finished_song=0
     n_waiting=0
-    print(f"IT IS 4AM (Day {i+1})\n")
+    print(f"\nIT IS 4AM (Day {i+1})\n(minute decimals signify hundredths for now)")
     #run the simulation once
     env = simpy.Environment()
     env.process(setup(env, Songbird_capacity, MEAN_SING, BETWEEN_BIRDS))
     env.run(until=END_T)
-    #description at end of shift
+    #description at end of hour
     print(f"Number of birds finished singing at 5:00AM on day {i + 1}: {n_finished_song}.\n")
     print(f"Number of birds not finished singing at 5:00AM on day {i + 1}: {n_waiting}.\n")
     #feed generated data into variables and report/do mild stats at end of day
